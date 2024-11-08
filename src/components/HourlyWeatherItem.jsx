770ab1f8615e685e926d72ed0792880fe1a5a96c
Mohamed Amine <ma.abahmane@gmail.com>
2024-11-08 19:03:01 +0100

    Add weather API integration and update components for dynamic data

--- src/components/HourlyWeatherItem.jsx
+++ src/components/HourlyWeatherItem.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
 
-const HourlyWeatherItem = () => {
+const HourlyWeatherItem = ({weeklyWeather}) => {
+
+ 
+
   return (
     <li className="weather-item">
         <p className="time"> 00:00 </p>
-        <img src="src/assets/icons/clouds.svg" alt="" className="weather-icon" />
+        <img src="src/assets/icons/rain.svg" alt="" className="weather-icon" />
         <p className="temperature">20°</p>
     </li>
   )
